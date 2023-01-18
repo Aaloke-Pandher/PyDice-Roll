f1f9c033547ed9447683a7420ff58953115f1f06
Aaloke-Pandher <a.pandher9@share.epsb.ca>
2023-01-18 14:04:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,35 @@ while loop:
     selection = input("Enter Selection (1-5): ") 
 
     # Action from Menu 
-    dice1 = random.randrange(1,7) 
-    dice2 = random.randrange(1,7)
-    if selection == "1":
+    if selection == "1": 
+        dice1 = random.randrange(1,7) 
+        dice2 = random.randrange(1,7)
         print(dice1, dice2, "sum:", int(dice1) + int(dice2)) 
-    elif selection == "2": 
+    elif selection == "2":  
         n = 0 
         for n in range(5): 
+            dice1 = random.randrange(1,7) 
+            dice2 = random.randrange(1,7) 
             print(dice1, dice2, "sum:", int(dice1) + int(dice2)) 
+    elif selection == "3": 
+        n = input("Enter a number:")
+        i = 0 
+        for i in range(int(n)):
+            dice1 = random.randrange(1,7) 
+            dice2 = random.randrange(1,7)
+            print(dice1, dice2, "sum:", int(dice1) + int(dice2))
+            i += 1
+    elif selection == "4": 
+        i = 0
+        roll = True
+        while roll: 
+            dice1 = random.randrange(1,7) 
+            dice2 = random.randrange(1,7)
+            print(dice1, dice2, "sum:", int(dice1) + int(dice2))
+            i += 1 
+            if dice1 == 1 and dice2 == 1:
+                roll = False 
+                print("You got snake eyes after", i, "rolls!")
     elif selection == "5": 
         print("Goodbye")
         loop = False
